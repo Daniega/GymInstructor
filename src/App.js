a37e747545bec0faf06eb54cbcacec52ac9b2caa
Daniega <daniel.gaishuber@gmail.com>
2018-01-21 14:34:59 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './69840.png';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to Gym Instructor App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
